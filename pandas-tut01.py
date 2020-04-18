3a2ca38c835a97c18c54469f786844a1c0431d9c
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-18 14:36:14 +0700

    1

---
+++ pandas-tut01.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+anggota_keluarga = pd.DataFrame({
+                    "Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
+                    "Usia": [31, 28, 1],
+                    "Berat Badan": [70,65,10]})
+
+dict_anggota = {"Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
+                "Usia": [31, 28, 1],
+                "Berat Badan": [70,65,10]}

ffafa9ddade2e27e98da0474356db99f9a36d2b8
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-18 14:47:20 +0700

    2

--- pandas-tut01.py
+++ pandas-tut01.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-anggota_keluarga = pd.DataFrame({
-                    "Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
-                    "Usia": [31, 28, 1],
-                    "Berat Badan": [70,65,10]})
+df = pd.DataFrame({
+    "Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
+    "Usia": [31, 28, 1],
+    "Berat Badan": [70,65,10]})
 
-dict_anggota = {"Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
-                "Usia": [31, 28, 1],
-                "Berat Badan": [70,65,10]}
+anggota = {"Nama": ['Gilang Samudra', 'Fitria Malinda', 'Maryam Arisha'],
+           "Usia": [31, 28, 1],
+           "Berat Badan": [70,65,10]}
+
+maxi = df["Berat Badan"].max()
+maxi2 = pd.Series(anggota["Berat Badan"]).max()
+print(maxi)
+print(maxi2)

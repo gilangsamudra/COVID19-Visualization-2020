68f14aa75b1838edaeefd48583a14c1f7feca2bb
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-19 23:08:39 +0700

    6

--- pandas-tut01.py
+++ pandas-tut01.py
@@ -19,42 +19,44 @@ death = df.pivot(index='Date', columns='Country', values='Deaths')
 frate = df.pivot(index='Date', columns='Country', values='FRates')
 crate = df.pivot(index='Date', columns='Country', values='CRates')
 
-plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 plt.figure(dpi=200)
-plot = covid19.plot(title="COVID-19 Infection Number", fontsize=15,
-                    figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
+plot = covid19.plot(grid=False, title="COVID-19 Infection Number", fontsize=15,
+                    figsize=(12, 8), linewidth=5, legend=False, ax=plt.gca())
+plot.grid(b=True, which='major', axis='y', ls='--', lw=.5, c='k', alpha=.3)
 plot.set_xlabel('Dates')
-plot.set_ylabel('# of Infected')
 for country in negara:
     plot.text(x=covid19.index[-1], y=int(covid19[country].tail(1)),
-              s=int(covid19[country].tail(1)), fontsize=15)
+              s=country+": "+str(int(covid19[country].tail(1))), fontsize=15)
+plot.text(x=covid19.index[1], y=-2000, s='Source: https://github.com/datasets/\
+covid-19/blob/master/data/countries-aggregated.csv', fontsize=15)
+plot.text(x=covid19.index[1], y=-2300, s="by: GSK", fontsize=15)
+# plt.figure(dpi=200)
+# plot = death.plot(title="COVID-19 Death Number", fontsize=15,
+#                   figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
+# plot.set_xlabel('Dates')
+# plot.set_ylabel('# of Death')
+# for country in negara:
+#     plot.text(x=death.index[-1], y=int(death[country].tail(1)),
+#               s=int(death[country].tail(1)),
+#               fontsize=15)
 
-plt.figure(dpi=200)
-plot = death.plot(title="COVID-19 Death Number", fontsize=15,
-                  figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
-plot.set_xlabel('Dates')
-plot.set_ylabel('# of Death')
-for country in negara:
-    plot.text(x=death.index[-1], y=int(death[country].tail(1)),
-              s=int(death[country].tail(1)),
-              fontsize=15)
+# plt.figure(dpi=200)
+# plot = frate.plot(title="COVID-19 Fatality Rate", fontsize=15,
+#                   figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
+# plot.set_xlabel('Dates')
+# plot.set_ylabel('Fatality Rate')
+# for country in negara:
+#     plot.text(x=frate.index[-1], y=float(frate[country].tail(1)),
+#               s=float("%.2f" % frate[country].tail(1)),
+#               fontsize=15)
 
-plt.figure(dpi=200)
-plot = frate.plot(title="COVID-19 Fatality Rate", fontsize=15,
-                  figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
-plot.set_xlabel('Dates')
-plot.set_ylabel('Fatality Rate')
-for country in negara:
-    plot.text(x=frate.index[-1], y=float(frate[country].tail(1)),
-              s=float("%.2f" % frate[country].tail(1)),
-              fontsize=15)
-
-plt.figure(dpi=200)
-plot = crate.plot(title="COVID-19 Cured Rate", fontsize=15,
-                  figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
-plot.set_xlabel('Dates')
-plot.set_ylabel('# of Death')
-for country in negara:
-    plot.text(x=crate.index[-1], y=float(crate[country].tail(1)),
-              s=float("%.2f" % crate[country].tail(1)),
-              fontsize=15)
+# plt.figure(dpi=200)
+# plot = crate.plot(title="COVID-19 Cured Rate", fontsize=15,
+#                   figsize=(12, 8), linewidth=5, legend=True, ax=plt.gca())
+# plot.set_xlabel('Dates')
+# plot.set_ylabel('# of Death')
+# for country in negara:
+#     plot.text(x=crate.index[-1], y=float(crate[country].tail(1)),
+#               s=float("%.2f" % crate[country].tail(1)),
+#               fontsize=15)
